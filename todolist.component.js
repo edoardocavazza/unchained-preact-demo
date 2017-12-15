427604183899ab838fa83657a16c99a048f46b49
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-12-15 11:33:12 +0100

    feat: make it persistent

--- todolist.component.js
+++ todolist.component.js
@@ -1,13 +1,16 @@
 import { h, Component } from 'preact';
 
+const localTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+
 export default class TodoList extends Component {
-    state = { todos: [], text: '' };
+    state = { todos: localTodos, text: '' };
     setText = e => {
         this.setState({ text: e.target.value });
     };
     addTodo = () => {
         let { todos, text } = this.state;
         todos = todos.concat({ text });
+        localStorage.setItem('todos', JSON.stringify(todos));
         this.setState({ todos, text: '' });
     };
     render({ }, { todos, text }) {
